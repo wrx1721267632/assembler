89e0e734cc34cc5328c7fa55ff58abe650be2a38
wrx1721267632 <1721267632@qq.com>
2016-06-15 15:29:42 +0800

    test

---
+++ exp13-2.asm
@@ -0,0 +1,34 @@
+assume cs:code
+
+code segment
+ start:
+     mov ax,cs
+     mov ds,ax
+     mov si,offset do0
+     mov ax,0
+     mov es,ax
+     mov di,200h
+     mov cx,offset do0end-offset do0
+     cld 
+     rep movsb
+
+     mov word ptr es:[7ch*4],200h
+     mov word ptr es:[7ch*4+2],0
+
+     mov ax,4c00h
+     int 21h
+ do0:
+     push bp
+
+     dec cx
+     jcxz ok
+     mov bp,sp
+     add [bp+2],bx
+ ok:
+     pop bp
+     iret
+ do0end:
+     nop
+code ends
+
+end start
