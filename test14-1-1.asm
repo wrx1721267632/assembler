fcdbc9cf3ac4f7279837610027dc34ba34b1bce9
wrx1721267632 <1721267632@qq.com>
2016-06-17 22:00:59 +0800

    test

---
+++ test14-1-1.asm
@@ -0,0 +1,16 @@
+assume cs:code
+
+code segment
+ start:
+     mov al,2
+     out 70h,al
+     in al,71h
+     mov dl,al
+     mov ah,2
+     int 21h
+
+     mov ax,4c00h
+     int 21h
+code ends
+
+end start
